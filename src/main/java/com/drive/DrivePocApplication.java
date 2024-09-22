af1e9aa5ec756b7931a224b304b08cd485f13589
Maciej Słowiak <slowiakmaciej@gmail.com>
2024-09-22 09:12:12 +0200

    Project init

---
+++ src/main/java/com/drive/DrivePocApplication.java
@@ -0,0 +1,13 @@
+package com.drive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivePocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drive/DrivePocApplicationTests.java
@@ -0,0 +1,14 @@
+package com.drive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//check if application starts
+	}
+
+}
